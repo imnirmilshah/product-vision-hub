6589ab3ba457475aa743ee185387a53f65eeab27
Nirmil <57025305+imnirmilshah@users.noreply.github.com>
2026-02-18 18:00:45 -0500

    Update App.tsx
    updated to have page public through github

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/product-vision-hub">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/blog" element={<BlogLanding />} />
